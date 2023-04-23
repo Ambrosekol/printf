6dfc5505faa7cd62a2693622a0fea8d111457330
ambrose <kolus7381@gmail.com>
2023-04-23 19:43:50 +0100

    [200~#include

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+/**
+ * struct checker - maps the specifiers for printf
+ * @id: char pointer of the specifier
+ * @func: type pointer to function for the conversion
+ *
+ */
+typedef struct checker
+{
+	char *id;
+	int (*func)();
+} check_match;
+int putstrng(va_list list);
+int putper(void);
+int print_char(va_list arg);
+#endif
