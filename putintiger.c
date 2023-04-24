3860290d2469ed179f2e8b4871638d4eaac6fb93
ambrose <kolus7381@gmail.com>
2023-04-24 22:30:17 +0100

    betty changes

--- putintiger.c
+++ putintiger.c
@@ -7,44 +7,44 @@
 int putint(va_list list)
 {
 	int num, tmp, len, i, outlen;
-    char *p;
+	char *p;
 
-    num = va_arg(list, int);
-    tmp = abs(num);
+	num = va_arg(list, int);
+	tmp = abs(num);
 	len = 0;
 	i = 0;
-    outlen = 0;
+	outlen = 0;
 	if (num == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-    else
-    {
-        while (tmp != 0)
-        {
-            tmp /= 10;
-            len++;
-        }
-        if (num < 0)
-        {
-            p = malloc(sizeof(char) * (len + 2));
-            if (p == NULL)
-                return (0);
-            sprintf(p, "%d", num);
-        }
-        else
-        {
-            p = malloc(sizeof(char) * (len + 1));
-            if (p == NULL)
-                return (0);
-            sprintf(p, "%d", num);
-        }
-        for (i = 0; i < (int) strlen(p); i++)
-        {
-            _putchar(p[i]);
-            outlen++;
-        }
-    }
-    return (outlen);
+	else
+	{
+		while (tmp != 0)
+		{
+			tmp /= 10;
+			len++;
+		}
+		if (num < 0)
+		{
+			p = malloc(sizeof(char) * (len + 2));
+			if (p == NULL)
+				return (0);
+			sprintf(p, "%d", num);
+		}
+		else
+		{
+			p = malloc(sizeof(char) * (len + 1));
+			if (p == NULL)
+				return (0);
+			sprintf(p, "%d", num);
+		}
+		for (i = 0; i < (int) strlen(p); i++)
+		{
+			_putchar(p[i]);
+			outlen++;
+		}
+	}
+	return (outlen);
 }
